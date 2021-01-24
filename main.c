c2ef978b343a85a2000a82e81c246730086f3999
Donika Ilieva <donika.a.ilieva@gmail.com>
2021-01-24 13:42:10 +0200

    Update main.c

--- main.c
+++ main.c
@@ -5,53 +5,47 @@
 Над 181 см до 205 см. – висок ръст
 Извън тези граници – изключително висок ръст*/
 #include <stdio.h>
-#include <stdbool.h> 
+#include <stdbool.h>
+#include <ctype.h> 
 
 void heightAnalyze(unsigned int height);
 bool inputValidations(unsigned int height);
 
 int main(void) {
-    int height=0;
+    unsigned int height = 0;
         
-        printf("             Enter your height in cm:\n ");
-        printf("========================================================\n");
-        scanf("%d", &height);
-        while (height != 0) {
-            while (!inputValidations(height)){
-                scanf("%d", &height); 
-            }
-            heightAnalyze(height);
-            height=0;
-            printf("If you want to continue enter height\nor type \"Stop\" \"0\" to end.\n");
-            scanf("%d", &height); 
-        }
-        printf("Bye!\n");
-        printf("--------------------------------------------------------\n");
+    printf("             Enter your height in cm:\n ");
+    printf("========================================================\n");
+    scanf(" %d", &height);
+    
+    do {
+        heightAnalyze(height);
+        printf("If you want to continue enter height\nor type \"0\" to end.\n");
+        scanf(" %d", &height); 
+    } while (inputValidations(height) > 0);
+    
+    printf("Bye!\n");
+    printf("--------------------------------------------------------\n");
         
     return 0;
 }
 
 bool inputValidations(unsigned int height) {
-    if(height >= 0x41 && height <= 0x7A) {
-        printf("Invalid input!\nYou can use only numbers for your height!\n");
-        return false;
+    if(height <= 0) {
+        printf("Invalid input!\nEnter your height again!\n");
+        scanf(" %d", &height);
     } 
-    if(height <= 5) {
-        printf("Nobody is born 5 cm?!\n");
-        printf("Enter your real height: \n");
-        return false;
-    }
     return true;
 }
 
 void heightAnalyze(unsigned int height) {
     if (height <= 145) {
-        printf("You are a person of short stature.\n");
+        printf("You are a person of short stature.\n\n");
     } else if(height >= 146 && height <= 180) {
-        printf("You are an average person in height.\n");
+        printf("You are an average person in height.\n\n");
     } else if(height >= 181 && height <= 205) {
-        printf("You are a tall person!\n");
+        printf("You are a tall person!\n\n");
     } else {
-        printf("You are extremely tall!\n");
+        printf("You are extremely tall!\n\n");
     }
 }
